b854c90d8885675bde0f550e615b5f691f537849
Ben Cull <bjcull@gmail.com>
2016-09-28 08:41:29 +0200

    Bumped version again, forgot to build

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,27 +1,25 @@
 "use strict";
 var globals_1 = require('./globals');
+exports.Globals = globals_1.Globals;
 var util_1 = require('./util');
+exports.Utils = util_1.Utils;
 var ingredient_1 = require('./ingredient');
+exports.Ingredient = ingredient_1.Ingredient;
 var fermentable_1 = require('./fermentable');
+exports.Fermentable = fermentable_1.Fermentable;
 var spice_1 = require('./spice');
+exports.Spice = spice_1.Spice;
 var yeast_1 = require('./yeast');
+exports.Yeast = yeast_1.Yeast;
 var mashStep_1 = require('./mashStep');
+exports.MashStep = mashStep_1.MashStep;
+exports.MashStepType = mashStep_1.MashStepType;
 var mash_1 = require('./mash');
+exports.Mash = mash_1.Mash;
 var recipe_1 = require('./recipe');
+exports.Recipe = recipe_1.Recipe;
 var guidService_1 = require('./guidService');
+exports.GuidService = guidService_1.GuidService;
 var beerxml_1 = require('./beerxml');
-module.exports = {
-    Globals: globals_1.Globals,
-    Utils: util_1.Utils,
-    Ingredient: ingredient_1.Ingredient,
-    Fermentable: fermentable_1.Fermentable,
-    Spice: spice_1.Spice,
-    Yeast: yeast_1.Yeast,
-    Mash: mash_1.Mash,
-    MashStep: mashStep_1.MashStep,
-    MashStepType: mashStep_1.MashStepType,
-    Recipe: recipe_1.Recipe,
-    GuidService: guidService_1.GuidService,
-    BeerXml: beerxml_1.BeerXml
-};
+exports.BeerXml = beerxml_1.BeerXml;
 //# sourceMappingURL=index.js.map
